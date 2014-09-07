d63838d529cf187e43ed942efa6764659b251722
Daniel Lo Nigro <danlo@fb.com>
2014-09-06 22:08:05 -0700

    Update 2014-09-07T05:08:05.338Z

---
+++ magic-loader.js
@@ -0,0 +1,34 @@
+/** @preserve
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+(function(document) {
+  var scripts = [
+    'http://fb.me/react-0.10.0.min.js',
+    'http://facebook.github.io/react/js/JSXTransformer.js',
+    'http://facebook.github.io/react/js/html-jsx-lib.js',
+    'http://reactjs.github.io/react-magic/magic.js'
+  ];
+
+  // Sometimes Chrome was loading the scripts in the wrong order (lolwat)
+  // We need to enforce order, so manually chain the loading.
+  function loadNext() {
+    if (scripts.length === 0) {
+      return;
+    }
+    var nextScript = scripts.shift();
+    var script = document.createElement('script');
+    script.src = nextScript;
+    script.onload = loadNext;
+    document.body.appendChild(script);
+  }
+  loadNext();
+
+  return false;
+})(document);
